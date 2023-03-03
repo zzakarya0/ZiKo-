4bd02b8f59a757cecc3bc3024b95eb416229706f
zzakarya0 <zzakarya0731@gmail.com>
2023-03-03 18:55:19 -0500

    empty project intialization

---
+++ Source/Ziko/ZikoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZikoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZIKO_API AZikoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
