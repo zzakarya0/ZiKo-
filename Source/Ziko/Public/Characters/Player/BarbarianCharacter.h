a4732b0134e63ed6a5422daacdecef43ad70e714
yaelh <elhaeey@gmail.com>
2023-03-27 04:22:36 +0100

    updated cpp file

--- Source/Ziko/Public/Characters/Player/BarbarianCharacter.h
+++ Source/Ziko/Public/Characters/Player/BarbarianCharacter.h
@@ -1,7 +1,28 @@
 ﻿#pragma once
+#include "BaseCharacter.h"
+#include "CoreMinimal.h"
 
-class BarbarianCharacter
+UCLASS()
+class ZIKO_API ABarbarianCharacter : public  ABaseCharacter
 {
+	GENERATED_BODY();
 public:
+	ABarbarianCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void BaseAttack() override;
+
+	/*Perform character base attack*/
+	virtual void FirstAbilityAttack() override;
+
+	/*Perform character base attack*/
+	virtual void SecondAbilityAttack() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 	
 };
